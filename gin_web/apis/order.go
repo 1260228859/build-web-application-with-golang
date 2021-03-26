26c3e450eb43a0aaa0154f7d0715784863495cec
Evankao <1260228859@qq.com>
2021-03-26 13:05:08 +0800

    feat: golang换源

--- gin_web/apis/order.go
+++ gin_web/apis/order.go
@@ -1,29 +1,29 @@
 package apis
 
 import (
- "net/http"
- "log"
- "fmt"
- "github.com/gin-gonic/gin"
- . "./models"
+	"build-web-application-with-golang/gin_web/models"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func IndexApi(c *gin.Context) {
- c.String(http.StatusOK, "It works")
+	c.String(http.StatusOK, "It works")
 }
 
 func AddOrderApi(c *gin.Context) {
- id := c.Request.FormValue("id")
- code := c.Request.FormValue("code")
- 
- p := Order{id, code}
+	id := c.Request.FormValue("id")
+	code := c.Request.FormValue("code")
+	p := Order{id, code}
 
- ra, err := p.AddOrder()
- if err != nil {
-  log.Fatalln(err)
- }
- msg := fmt.Sprintf("insert successful %d", ra)
- c.JSON(http.StatusOK, gin.H{
-  "msg": msg,
- })
+	ra, err := p.AddOrder()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	msg := fmt.Sprintf("insert successful %d", ra)
+	c.JSON(http.StatusOK, gin.H{
+		"msg": msg,
+	})
 }
